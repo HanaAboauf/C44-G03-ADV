5739e0482798d0b6433f96b621b15abd2896eecb
Hana <hanamarwasadek2004@gmail.com>
2025-08-22 21:45:43 +0300

    solution of question01

---
+++ Assignment2/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question01
+
+            //Console.WriteLine("Enter the size of the array: ");
+
+            //int size = int.Parse(Console.ReadLine()!);
+
+            //int[] numbers = new int[size];
+
+            //int counter = 0;
+
+            //Console.WriteLine("Enter enter the number of the queries: ");
+
+            //int queries = int.Parse(Console.ReadLine()!);
+            //for (int i = 0; i < size; i++)
+            //{
+            //    Console.WriteLine($"Enter the number {i + 1}: ");
+            //    numbers[i] = int.Parse(Console.ReadLine()!);
+            //}
+
+            //for (int i = 0; i < queries; i++)
+            //{
+            //    Console.WriteLine($"Enter the query 0{i + 1}: ");
+            //    int query = int.Parse(Console.ReadLine()!);
+            //    foreach (var item in numbers)
+            //    {
+            //        if (item >= query)
+            //        {
+            //            counter++;
+            //        }
+
+            //    }
+            //    Console.WriteLine($"The number of elements greater than or equal to 0{query} is: {counter}");
+            //    counter = 0;
+
+            //}
+            #endregion
+        }
+    }
+}

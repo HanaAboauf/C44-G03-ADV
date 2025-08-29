0fe88ecf49ceb50c349bbf80b7983c3702990020
Hana <hanamarwasadek2004@gmail.com>
2025-08-29 23:40:32 +0300

    solution of question02

--- Assignment04/Program.cs
+++ Assignment04/Program.cs
@@ -6,20 +6,49 @@
         {
             #region Question01
 
-            int[] numbers = { 1, 2, 2, 3, 4, 4, 4, 5 };
+            //int[] numbers = { 1, 2, 2, 3, 4, 4, 4, 5 };
 
-            Dictionary<int, int> frequencyTable = new Dictionary<int, int>();
-            foreach (int number in numbers)
-            {
-                if (frequencyTable.ContainsKey(number))
-                {
-                    frequencyTable[number]++;
-                }
-                else
-                {
-                    frequencyTable[number] = 1;
-                }
-            }
+            //Dictionary<int, int> frequencyTable = new Dictionary<int, int>();
+            //foreach (int number in numbers)
+            //{
+            //    if (frequencyTable.ContainsKey(number))
+            //    {
+            //        frequencyTable[number]++;
+            //    }
+            //    else
+            //    {
+            //        frequencyTable[number] = 1;
+            //    }
+            //}
+            //foreach (var kvp in frequencyTable)
+            //{
+            //    Console.WriteLine($"Number: {kvp.Key}, Frequency: {kvp.Value}");
+            //}
+            #endregion
+
+            #region Question02
+
+            //Dictionary<string, int> hashtable = new Dictionary<string, int>() {
+            //    { "a", 10 },
+            //    { "b", 20 },
+            //    {"c", 15 }
+            //};
+
+            //string keyWithHighestValue = null;
+
+            //int highestValue = int.MinValue;
+
+            //foreach (var kvp in hashtable)
+            //{
+            //    if (kvp.Value > highestValue)
+            //    {
+            //        highestValue = kvp.Value;
+            //        keyWithHighestValue = kvp.Key;
+            //    }
+            //}
+            //Console.WriteLine($"Key with highest value: {keyWithHighestValue}, Value: {highestValue}");
+
+
             #endregion
         }
     }

5cc257e1f9701e902a7e2349f2a4e2a3a7308b53
Hana <hanamarwasadek2004@gmail.com>
2025-08-29 23:37:08 +0300

    solution of question01

---
+++ Assignment04/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question01
+
+            int[] numbers = { 1, 2, 2, 3, 4, 4, 4, 5 };
+
+            Dictionary<int, int> frequencyTable = new Dictionary<int, int>();
+            foreach (int number in numbers)
+            {
+                if (frequencyTable.ContainsKey(number))
+                {
+                    frequencyTable[number]++;
+                }
+                else
+                {
+                    frequencyTable[number] = 1;
+                }
+            }
+            #endregion
+        }
+    }
+}
